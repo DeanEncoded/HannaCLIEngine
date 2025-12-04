61e0b4c39e1af6a3aa8b37c1d8e67bffa6bb110d
Dean Mudakureva <45594986+DeanEncoded@users.noreply.github.com>
2025-12-04 06:25:44 +0200

    Refactor Exporter for improved JSON export and file handling
    Rewrote the Exporter class to use a dedicated JsonBuilder for generating JSON output, improving readability and maintainability. Added robust file export logic with optional encryption for game distribution. Deprecated old methods in favor of new, clearer APIs and enhanced null handling and string escaping throughout the export process.

--- Hanna-Studio/Helpers/Exporter.cs
+++ Hanna-Studio/Helpers/Exporter.cs
@@ -1,188 +1,362 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Hanna_Studio
 {
-    class Exporter
+    /// <summary>
+    /// Exports game data to JSON format for the Hanna CLI Engine.
+    /// </summary>
+    public class Exporter
     {
+        private const string NullPlaceholder = "hnull";
 
-        //TODO: DON'T ALLOW EXPORT WHEN CERTAIN CONDITIONS ARE MET
+        private readonly string _gameTitle;
+        private readonly string _gameAuthor;
+        private readonly string _gameDesc;
+        private readonly List<string> _gameContainers;
+        private readonly string _startSequence;
+        private readonly Dictionary<string, Sequence> _sequences;
 
-        string gameTitle;
-        string gameAuthor;
-        string gameDesc;
-
-        List<String> gameContainers;
-        string startSequence;
-
-        Dictionary<string, Sequence> sequences;
-        public Exporter(string gameTitle, string gameAuthor, string gameDesc, Dictionary<string, Sequence> sqs, List<string> gameContainers, string startSequence)
+        public Exporter(
+            string gameTitle,
+            string gameAuthor,
+            string gameDesc,
+            Dictionary<string, Sequence> sequences,
+            List<string> gameContainers,
+            string startSequence)
         {
-            // init export stuff
-            this.gameTitle = gameTitle;
-            this.gameAuthor = gameAuthor;
-            this.gameDesc = gameDesc;
-            sequences = sqs;
-            this.gameContainers = gameContainers;
-            this.startSequence = startSequence;
+            _gameTitle = gameTitle ?? string.Empty;
+            _gameAuthor = gameAuthor ?? string.Empty;
+            _gameDesc = gameDesc ?? string.Empty;
+            _sequences = sequences ?? new Dictionary<string, Sequence>();
+            _gameContainers = gameContainers ?? new List<string>();
+            _startSequence = startSequence ?? string.Empty;
         }
 
+        /// <summary>
+        /// Generates the JSON export string for the game.
+        /// </summary>
+        public string GenerateExportString()
+        {
+            var json = new JsonBuilder();
 
-        public string generateExportString() {
+            json.BeginObject();
+            json.AddProperty("gameTitle", _gameTitle);
+            json.AddProperty("gameAuthor", _gameAuthor);
+            json.AddProperty("gameDesc", _gameDesc);
+            json.AddPropertyName("gameContainers");
+            WriteContainersArray(json);
+            json.AddProperty("startSq", _startSequence);
+            json.AddPropertyName("sequences");
+            WriteSequencesArray(json);
+            json.EndObject();
 
-
-            string estring_gameContainers = @" ""hnull"" ";
-            // generate containers string!
-            if (gameContainers.Count > 0) {
-                string tempString = "";
-                bool firstIn = false;
-                foreach (String c in gameContainers) {
-                    if (!firstIn)
-                    { // don't add a comma!
-                        tempString += @" """ + c + @"""";
-                        firstIn = true;
-                    }
-                    else { // add a comma to the beginning
-                        tempString += @",""" + c + @"""";
-                    }
-                }
-                estring_gameContainers = tempString;
-            }
-
-            string estring_gameSequences = @"";
-            bool isFirstSq = true;
-            // loop through all the sequences!
-            foreach (KeyValuePair<string, Sequence> sequence in sequences) {
-
-                //make a meta string for this sequence
-                string tempsqMeta = @"";
-                if (!isFirstSq)
-                { // put a comma on tempsqMeta
-                    tempsqMeta += @",";
-                }
-                else {
-                    isFirstSq = false;
-                }
-                tempsqMeta += @"{""sqId"": """+ sequence.Value.id +@""",
-                                ""sqType"": """+ sequence.Value.type +@""",
-                                ""mainText"": """+ sequence.Value.mainText.Replace("\"","\\\"") +@""",
-                                ""secondaryText"": """+ sequence.Value.secondaryText.Replace("\"", "\\\"") + @""",
-                                ""choices"":[";
-                // now lets loop through the choices of this sequence
-
-                // what about if it doesn't have choices????
-                if (sequence.Value.choices.Count < 1) {
-                    // just close the bracket
-                }
-                bool isFirstChoice = true;
-                foreach (KeyValuePair<string,Choice> choice in sequence.Value.choices) {
-                    // generate choice condition string
-                    string estringChoiceCondition = @"
-                                    {
-                                    ""container"": ""hnull"",
-                                    ""value"": ""hnull"" }";
-                    if (choice.Value.condition.Count > 0) {
-                        estringChoiceCondition = @"
-                                    {
-                                    ""container"": """ + choice.Value.condition[0] + @""",
-                                    ""value"": """ + choice.Value.condition[1] + @""" }";
-                    }
-
-                    // generate choice containerAdd strings
-                    // if containrAdd container has nothing then it's null
-                    string estringChoiceContainerAdd = @"
-                                    {
-                                    ""container"": ""hnull"",
-                                    ""value"": ""hnull""}";
-                    if (choice.Value.containerAdd != null)
-                    {
-                        estringChoiceContainerAdd = @"
-                                    {
-                                    ""container"": """ + choice.Value.containerAdd + @""",
-                                    ""value"": """ + choice.Value.containerAddValue + @"""}";
-                    }
-
-
-                    // check whether it's our first choice. So as to put a comma at the start
-                    string tempChoiceMeta = @"";
-                    if (isFirstChoice){
-                        // dont add the comma
-                        isFirstChoice = false;
-                    }
-                    else {
-                        // add the comma
-                        tempChoiceMeta += @",";
-                    }
-             
-            
-                    tempChoiceMeta += @"{
-                                ""choiceLetter"": """+ choice.Key +@""",
-                                ""choiceType"": """+ choice.Value.type +@""",
-                                ""choiceCondition"": "+ estringChoiceCondition +@",
-                                ""choiceText"": """+ choice.Value.text.Replace("\"", "\\\"") + @""",
-                                ""outcomeText"": """+ choice.Value.outcometext.Replace("\"", "\\\"") + @""",
-                                ""containerAdd"": "+ estringChoiceContainerAdd +@",
-                                ""containerDispose"": {
-                                ""container"": ""null""
-                                },
-                                ""nextSq"": """+ choice.Value.nextSq +@"""
-                                }";
-                    // push tempChoiceMeta the sequence string
-                    tempsqMeta += tempChoiceMeta;
-                }
-                tempsqMeta += @"]"; // close the choices bracket???
-
-                // puth tempsqMeta to estring_gameSequences
-                estring_gameSequences += tempsqMeta + @"}";
-
-            }
-
-            // generate sequences string!
-
-            string estring_closers = @"
-                ]
-            }";
-
-            string meta = @"
-               {
-              ""gameTitle"": """ + gameTitle + @""",
-              ""gameAuthor"":  """ + gameAuthor + @""",
-              ""gameDesc"": """ + gameDesc + @""",
-              ""gameContainers"": [ " + estring_gameContainers + @" ],
-              ""startSq"": """+ startSequence +@""",
-              ""sequences"":[" + estring_gameSequences + estring_closers;
-            return meta;
-
+            return json.ToString();
         }
 
-        public bool exportToFile(bool exportgame = false, string exportlocation = "export.json") // exportLocation is set to export.json at first.
+        /// <summary>
+        /// Exports the game data to a file.
+        /// </summary>
+        /// <param name="exportGame">If true, encrypts the output for game distribution (.hgm file).</param>
+        /// <param name="exportLocation">The file path for the export.</param>
+        /// <returns>True if export was successful, false otherwise.</returns>
+        public bool ExportToFile(bool exportGame = false, string exportLocation = "export.json")
         {
-            string exportString = generateExportString(); // representing the game json export string
             try
             {
-                if (exportgame) {
-                    // if we're exporting the game (into an .hgm file).. use exportLocation for the export.. otherwise, just use "export.json" for running inside the studio (This will happen if the parameter remains its default value)
-                    // if we're exporting the game. The contents have to be encrypted
-                    exportString = CrossPlatformAESEncryption.Helper.CryptoHelper.Encrypt(exportString, Helpers.KEYS.getExportKey());
-                    // done. the string is now encrypted
+                string exportString = GenerateExportString();
+
+                if (exportGame)
+                {
+                    exportString = CrossPlatformAESEncryption.Helper.CryptoHelper.Encrypt(
+                        exportString,
+                        Helpers.KEYS.getExportKey());
                 }
-                System.IO.StreamWriter writer = new System.IO.StreamWriter(exportlocation); //open the file for writing.
-                writer.Write(exportString); //write the current date to the file. change this with your date or something.
-                writer.Close(); //remember to close the file again.
-                writer.Dispose(); //remember to dispose it from the memory.
+
+                File.WriteAllText(exportLocation, exportString, Encoding.UTF8);
                 return true;
             }
-            catch (Exception e) {
+            catch (Exception)
+            {
                 return false;
             }
-            
-
-        
-
         }
-}
 
+        private void WriteContainersArray(JsonBuilder json)
+        {
+            json.BeginArray();
+
+            if (_gameContainers.Count > 0)
+            {
+                foreach (var container in _gameContainers)
+                {
+                    json.AddArrayValue(container);
+                }
+            }
+            else
+            {
+                json.AddArrayValue(NullPlaceholder);
+            }
+
+            json.EndArray();
+        }
+
+        private void WriteSequencesArray(JsonBuilder json)
+        {
+            json.BeginArray();
+
+            foreach (var sequenceEntry in _sequences)
+            {
+                WriteSequence(json, sequenceEntry.Value);
+            }
+
+            json.EndArray();
+        }
+
+        private void WriteSequence(JsonBuilder json, Sequence sequence)
+        {
+            json.BeginObject();
+            json.AddProperty("sqId", sequence.id ?? string.Empty);
+            json.AddProperty("sqType", sequence.type ?? string.Empty);
+            json.AddProperty("mainText", sequence.mainText ?? string.Empty);
+            json.AddProperty("secondaryText", sequence.secondaryText ?? string.Empty);
+            json.AddPropertyName("choices");
+            WriteChoicesArray(json, sequence.choices);
+            json.EndObject();
+        }
+
+        private void WriteChoicesArray(JsonBuilder json, Dictionary<string, Choice> choices)
+        {
+            json.BeginArray();
+
+            if (choices != null)
+            {
+                foreach (var choiceEntry in choices)
+                {
+                    WriteChoice(json, choiceEntry.Key, choiceEntry.Value);
+                }
+            }
+
+            json.EndArray();
+        }
+
+        private void WriteChoice(JsonBuilder json, string choiceLetter, Choice choice)
+        {
+            json.BeginObject();
+            json.AddProperty("choiceLetter", choiceLetter ?? string.Empty);
+            json.AddProperty("choiceType", choice.type ?? string.Empty);
+
+            json.AddPropertyName("choiceCondition");
+            WriteCondition(json, choice.condition);
+
+            json.AddProperty("choiceText", choice.text ?? string.Empty);
+            json.AddProperty("outcomeText", choice.outcometext ?? string.Empty);
+
+            json.AddPropertyName("containerAdd");
+            WriteContainerAdd(json, choice.containerAdd, choice.containerAddValue);
+
+            json.AddPropertyName("containerDispose");
+            json.BeginObject();
+            json.AddProperty("container", "null");
+            json.EndObject();
+
+            json.AddProperty("nextSq", choice.nextSq ?? string.Empty);
+            json.EndObject();
+        }
+
+        private void WriteCondition(JsonBuilder json, List<string> condition)
+        {
+            json.BeginObject();
+
+            if (condition != null && condition.Count >= 2)
+            {
+                json.AddProperty("container", condition[0] ?? NullPlaceholder);
+                json.AddProperty("value", condition[1] ?? NullPlaceholder);
+            }
+            else
+            {
+                json.AddProperty("container", NullPlaceholder);
+                json.AddProperty("value", NullPlaceholder);
+            }
+
+            json.EndObject();
+        }
+
+        private void WriteContainerAdd(JsonBuilder json, string container, string value)
+        {
+            json.BeginObject();
+
+            if (!string.IsNullOrEmpty(container))
+            {
+                json.AddProperty("container", container);
+                json.AddProperty("value", value ?? NullPlaceholder);
+            }
+            else
+            {
+                json.AddProperty("container", NullPlaceholder);
+                json.AddProperty("value", NullPlaceholder);
+            }
+
+            json.EndObject();
+        }
+
+        #region Backward Compatibility
+
+        [Obsolete("Use GenerateExportString() instead")]
+        public string generateExportString() => GenerateExportString();
+
+        [Obsolete("Use ExportToFile() instead")]
+        public bool exportToFile(bool exportgame = false, string exportlocation = "export.json")
+            => ExportToFile(exportgame, exportlocation);
+
+        #endregion
+    }
+
+    /// <summary>
+    /// A simple JSON builder that creates properly formatted JSON output.
+    /// </summary>
+    internal class JsonBuilder
+    {
+        private readonly StringBuilder _sb = new StringBuilder();
+        private readonly Stack<JsonContext> _contextStack = new Stack<JsonContext>();
+        private int _indentLevel = 0;
+        private const string IndentString = "  ";
+
+        private enum JsonContext
+        {
+            Object,
+            Array
+        }
+
+        private bool NeedsSeparator => _sb.Length > 0 &&
+            !_sb.ToString().EndsWith("{") &&
+            !_sb.ToString().EndsWith("[") &&
+            !_sb.ToString().EndsWith("\n") &&
+            !_sb.ToString().EndsWith(": ");
+
+        public void BeginObject()
+        {
+            if (NeedsSeparator) AppendComma();
+            AppendIndent();
+            _sb.Append("{");
+            _sb.AppendLine();
+            _indentLevel++;
+            _contextStack.Push(JsonContext.Object);
+        }
+
+        public void EndObject()
+        {
+            _indentLevel--;
+            _sb.AppendLine();
+            AppendIndent();
+            _sb.Append("}");
+            if (_contextStack.Count > 0) _contextStack.Pop();
+        }
+
+        public void BeginArray()
+        {
+            _sb.Append("[");
+            _sb.AppendLine();
+            _indentLevel++;
+            _contextStack.Push(JsonContext.Array);
+        }
+
+        public void EndArray()
+        {
+            _indentLevel--;
+            _sb.AppendLine();
+            AppendIndent();
+            _sb.Append("]");
+            if (_contextStack.Count > 0) _contextStack.Pop();
+        }
+
+        public void AddProperty(string name, string value)
+        {
+            AddPropertyName(name);
+            _sb.Append(EscapeString(value));
+        }
+
+        public void AddPropertyName(string name)
+        {
+            if (NeedsSeparator) AppendComma();
+            AppendIndent();
+            _sb.Append(EscapeString(name));
+            _sb.Append(": ");
+        }
+
+        public void AddArrayValue(string value)
+        {
+            if (NeedsSeparator) AppendComma();
+            AppendIndent();
+            _sb.Append(EscapeString(value));
+        }
+
+        private void AppendComma()
+        {
+            _sb.Append(",");
+            _sb.AppendLine();
+        }
+
+        private void AppendIndent()
+        {
+            for (int i = 0; i < _indentLevel; i++)
+            {
+                _sb.Append(IndentString);
+            }
+        }
+
+        private static string EscapeString(string value)
+        {
+            if (value == null) return "null";
+
+            var sb = new StringBuilder();
+            sb.Append('"');
+
+            foreach (char c in value)
+            {
+                switch (c)
+                {
+                    case '"':
+                        sb.Append("\\\"");
+                        break;
+                    case '\\':
+                        sb.Append("\\\\");
+                        break;
+                    case '\b':
+                        sb.Append("\\b");
+                        break;
+                    case '\f':
+                        sb.Append("\\f");
+                        break;
+                    case '\n':
+                        sb.Append("\\n");
+                        break;
+                    case '\r':
+                        sb.Append("\\r");
+                        break;
+                    case '\t':
+                        sb.Append("\\t");
+                        break;
+                    default:
+                        if (c < ' ')
+                        {
+                            sb.AppendFormat("\\u{0:X4}", (int)c);
+                        }
+                        else
+                        {
+                            sb.Append(c);
+                        }
+                        break;
+                }
+            }
+
+            sb.Append('"');
+            return sb.ToString();
+        }
+
+        public override string ToString() => _sb.ToString();
+    }
 }
